0f18590ae219c795350ab56240e847d5dea42698
georgiamcg <30060569@lcwc.ac.uk>
2020-10-05 18:49:05 +0000

    small alterations

--- main.c
+++ main.c
@@ -11,17 +11,20 @@ float DIVIDE(int num1, int num2, int num3,int num4, int num5, int num6, int num7
 
 int main(void) {
 
-printf("Enter a Calculation :");
 
   int i; 
   int num1=1; int num2=2; int num3=3; int num4=4; int num5=5; int num6=6; int num7=7; int num8=8; int num9=9;
 
- if PLUS(1+2);
- else if MINUS(2-1)
- else if MULTIPLY(2*1);
- else DIVIDE(3/1);
+ if {
+   PLUS(1+2);}
+ else if {
+   MINUS(2-1)}
+ else if {
+   MULTIPLY(2*1);}
+ else {
+   DIVIDE(3/1);}
 
-
+  printf("Enter a Calculation :");
   scanf("%d", &i); 
   printf( "\nAnswer: %d", i); 
 
