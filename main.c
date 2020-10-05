ef7f4d43219b7f9f8eb5f92193446a0acfe78c33
georgiamcg <30060569@lcwc.ac.uk>
2020-10-05 18:34:21 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+// function decleration
+int PLUS(int num1, int num2, int num3,int num4, int num5, int num6, int num7, int num8, int num9);
+
+int MINUS(int num1, int num2, int num3,int num4, int num5, int num6, int num7, int num8, int num9);
+
+int MULTIPLY(int num1, int num2, int num3,int num4, int num5, int num6, int num7, int num8, int num9);
+
+float DIVIDE(int num1, int num2, int num3,int num4, int num5, int num6, int num7, int num8, int num9);
+
+int main(void) {
+
+printf("Enter a Calculation :");
+
+  int i; 
+  int num1=1; int num2=2; int num3=3; int num4=4; int num5=5; int num6=6; int num7=7; int num8=8; int num9=9;
+
+ if PLUS(1+2);
+ else if MINUS(2-1)
+ else if MULTIPLY(2*1);
+ else DIVIDE(3/1);
+
+
+  scanf("%d", &i); 
+  printf( "\nAnswer: %d", i); 
+
+
+  return 0;
+}
