f0bf652cb218016d0d0ef546700b915c8bb92916
georgiamcg <30060569@lcwc.ac.uk>
2020-10-09 13:44:33 +0000

    CORRECT SET UP

--- main.c
+++ main.c
@@ -1,57 +1,70 @@
+
 #include <stdio.h>
 
 /* function declaration */
 int plus(int num1, int num2);
-int minus(int num3, int num4);
-int multiply(int num5, int num6);
-int divide(int num7, int num8);
+int minus(int num1, int num2);
+int multiply(int num1, int num2);
+int divide(int num1, int num2);
 
 
 int main(void) {
  // local vaiable definition
   int a = 10;
   int b = 20;
-  int c = 40;
-  int d = 30;
-  int e = 5;
-  int f = 6;
-  int g = 70;
-  int h = 35;
+  int ret;
 
-  int ADD; int TAKE; int TIMES; int SHARED;
+ // addition 
+  ret = plus(a, b);
+  printf("A+B = %d\n", ret);
 
-  ADD = plus(a, b);
-  TAKE = minus(c, d);
-  TIMES = multiply(e, f);
-  SHARED = divide(g, h);
+ // subtraction
+  ret = minus(a, b);
+  printf("A-B = %d\n", ret);
+
+  // multiply
+  ret = multiply(a, b);
+  printf("A*B = %d\n", ret);
+
+  // divide
+  ret = divide(a, b);
+  printf("A/B = %d\n", ret);
+
  
- // calling funtion
-  
-  printf("Enter an addition :");
-  scanf("%d", plus); 
-  printf( "\nAnswer: %d", ADD);
- 
-  printf("Enter a subtraction :");
-  scanf("%d", minus);
-  printf("\nAnswer: %d", TAKE);
-
-  printf("enter a multiplication :");
-  scanf("%d", multiply);
-  printf("\n Answer: %d", TIMES);
-
-  printf("Enter a division :");
-  scanf("%d", divide);
-  printf("\n Answer: %d", SHARED);
-
-
   return 0;
 }
-/* function returning the calculation */
+/* function returning the addition */
 int plus(int num1, int num2)
 {
   /* local variable decleration*/
-  int ADD;
+  int result;
+  result = num1+num2;
+  return result;
+}
 
-  
+/* function returning subtraction*/
+int minus(int num1, int num2)
+{
+  /* local variable decleration*/
+  int result;
+  result = num1-num2;
+  return result;
+}
+
+/* function returning multiplication*/
+int multiply(int num1, int num2)
+{
+  /* local varibale decleration*/
+  int result;
+  result = num1*num2;
+  return result;
+}
+
+/* function retuning division*/
+int divide(int num1, int num2)
+{
+  /* local variable decleration*/
+  int result;
+  result = num1/num2;
   return result;
 }
